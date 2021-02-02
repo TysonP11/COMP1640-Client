e0c78007580c42d10f474f15bb7c2893416e389d
ptson2 <son.phamthai@ifisolution.com>
2021-02-02 16:18:18 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
-function App() {
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import setAuthToken from './utils/setAuthToken';
+import Footer from './components/layout/Footer';
+import './App.css'
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+const App = () => {
+
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    //store.dispatch(loadUser());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Provider store={store}>
+      
+      <Router>
+        <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing}/>
+        <section className="container">
+        <Switch>
+          <Route path='/register' component={Register}/>
+          <Route path='/login' component={Login}/>
+        </Switch>
+        </section>
+        
+        </Fragment>
+      </Router>
+    </Provider>
+);
+};
 
 export default App;
